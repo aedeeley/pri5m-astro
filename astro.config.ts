ecc869b89872d9a80a9589fc88fcdb90c48c5c55
Alex Deeley <aedeeley@gmail.com>
2024-03-14 15:07:03 -0600

    add vercel/serverless and webaanalytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   site: process.env.CI
     ? 'https://astro-shadcn-ui-template.vercel.app'
     : 'http://localhost:4321',
